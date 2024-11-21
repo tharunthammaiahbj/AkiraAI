ba28088fca0911f8539599e3ab6721d18294a58a
tharunthammaiahbj <thammaiahtharun3@gmail.com>
2024-11-21 02:49:09 +0000

    import configuring

--- akiraai/utils/__init__.py
+++ akiraai/utils/__init__.py
@@ -1,12 +1,11 @@
 #For Proxy Rotation:
 
-from .proxy_rotation import (
-    ProxyBrokerCriteria, 
-    ProxySettings, 
-    Proxy, 
-    search_proxy_servers, 
-    _parse_proxy, 
-    _search_proxy, 
-    is_ipv4_address, 
-    parse_or_search_proxy
+from utils import (
+    Proxy,
+    search_proxy_servers,
+    _parse_proxy,
+    _search_proxy,
+    is_ipv4_address,
+    parse_or_search_proxy,
 )
+
