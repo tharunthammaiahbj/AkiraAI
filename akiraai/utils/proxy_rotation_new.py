15ddaf1544be10e322c897b6cd4bc0be025db0c1
tharunthammaiahbj <thammaiahtharun3@gmail.com>
2024-11-30 02:17:54 +0000

    proxy configuring

--- akiraai/utils/proxy_rotation_new.py
+++ akiraai/utils/proxy_rotation_new.py
@@ -5,6 +5,22 @@ from typing import List, Optional, Set, TypedDict
 import requests
 from fp.errors import FreeProxyException
 from fp.fp import FreeProxy
+from akiraai.utils.logging import get_logger
+import logging
+
+logger = get_logger("proxy-logger")
+
+# Configure the named logger
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler()  # Add a handler explicitly
+handler.setFormatter(logging.Formatter(
+    fmt=
+    (
+        "%(asctime)s [%(levelname)s] [%(name)s] "
+        "[%(filename)s:%(lineno)d] - %(message)s"
+    ),
+    datefmt="%Y-%m-%d %H:%M:%S"))
+logger.addHandler(handler)
 
 class ProxyFilter(TypedDict , total = False):
 
@@ -38,45 +54,49 @@ class ProxyFetcher:
         self.proxy_count =self.proxy_filter.get("proxy_count",5)
 
     def validated_proxy_list(self)->List[str]:
-        try:
-            proxy_list = self.proxybroker.get_proxy_list(
-                self.proxy_filter.get("outside_search", True)
-            )
-            abc = working_proxy_list(
-                proxybroker=self.proxybroker,
-                proxy_count=self.proxy_count,
-                outside_search=self.proxy_filter.get("outside_search",True)
-            )                        
-        except FreeProxyException as e:
-            raise Exception(f"Failed to fetch proxies: {str(e)}")
+         return active_proxy_list(
+              proxybroker=self.proxybroker,
+              proxy_count=self.proxy_count,
+              outside_search=self.proxy_filter.get("outside_search",True),
+         )
+         
     
     
 
 
 
-def working_proxy_list(proxybroker:FreeProxy, proxy_count: int, outside_search: bool):
+def active_proxy_list(proxybroker:FreeProxy, proxy_count: int, outside_search: bool)->List[str]:
+        
 
         valid_proxies = set()
+        for _ in range(2):
+            proxy_list = proxybroker.get_proxy_list(outside_search)
+            random.shuffle(proxy_list)
 
-        proxy_list = proxybroker.get_proxy_list(outside_search)
-        random.shuffle(proxy_list)
+            for element in proxy_list:
+                 proxy_url = {proxybroker.schema: f"http://{element}"}
 
-        for proxy in proxy_list:
-            proxy_url = {proxybroker.schema: f"http://{proxy}"}
+                 proxy_server =proxybroker._FreeProxy__check_if_proxy_is_working(proxy_url)
 
-            try:
-                proxy_server = proxybroker.__check_if_proxy_is_working(proxy_url)
-                if proxy_server:
-                    valid_proxies.add(proxy_server)
-                if len(valid_proxies) >= proxy_count:
-                    break
-            except requests.exceptions.RequestException:
-                continue
+                 if proxy_server:
+                      valid_proxies.add(proxy_server)
 
-        if len(valid_proxies)<proxy_count:
-            raise FreeProxyException("Not enough valid proxies found matching the criteria.")
+                 if len(valid_proxies) < proxy_count or not proxy_server:
+                    continue
+            
+            n = len(valid_proxies)
+            if len(valid_proxies)>=proxy_count:
+                 break
+            
+            elif n<proxy_count and outside_search:
+                 proxybroker.country_id=None
 
-        return list(valid_proxies)
+            else:
+                 raise FreeProxyException("missing proxy servers for criteria")
+        
+
+        return list(valid_proxies)[:proxy_count]
+            
 
         
         
--- tests/test2.py
+++ tests/test2.py
@@ -2,16 +2,15 @@ from akiraai.utils.proxy_rotation_new import ProxyFetcher,ProxyFilter
 
 
 filter : ProxyFilter = {
-    "allow_google":None,
     "anonymous":True,
     "country_preference":["US"],
-    "secure":True,
-    "outside_search_if_empty":True,
-    "time_out":7,
-    "max_proxies":6
+    "secure":False,
+    "outside_search":True,
+    "time_out":5,
+    "proxy_count":6
 }
 
 
 proxyserver = ProxyFetcher(proxy_filter=filter)
-print(f"ProxyList:{proxyserver.fetch_proxy_list()}")
+print(f"ProxyList:{proxyserver.validated_proxy_list()}")
 
